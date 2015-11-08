827094147dd51875cf258cddb58a8b222984477c
Nitant Dandekar <ndandekar@gatech.edu>
2015-11-08 00:59:23 -0500

    Add API for retrieving all questions from the database.

--- api/question.js
+++ api/question.js
@@ -4,6 +4,17 @@ var express = require('express');
 var database = require('../database');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+    database.Question.find({}, function(err, questions) {
+        if (err) {
+            res.sendStatus(500);
+        }
+        else {
+            res.sendStatus(200).json(questions);
+        }
+    });
+});
+
 router.put('/', bodyParser.json(), function(req, res) {
     var question = new database.Question(req.body);
 
