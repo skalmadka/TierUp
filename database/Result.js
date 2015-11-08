3e6777a16e05d2887b761ff667cdbfe7fabd6e36
Nitant Dandekar <ndandekar@gatech.edu>
2015-11-08 00:58:41 -0500

    Fix schema for results.

--- database/Result.js
+++ database/Result.js
@@ -1,8 +1,8 @@
 module.exports = function(mongoose) {
     var resultSchema = new mongoose.Schema({
         date: Date,
-        student: [{type: mongoose.Schema.Types.ObjectId, ref: 'Student'}],
-        question: [{type: mongoose.Schema.Types.ObjectId, ref: 'Question'}],
+        student: {type: mongoose.Schema.Types.ObjectId, ref: 'Student'},
+        question: {type: mongoose.Schema.Types.ObjectId, ref: 'Question'},
         type: String,
         correct: Boolean
     });
